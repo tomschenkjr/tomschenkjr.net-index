6f7e124755782928e886c29c4b06338276b4ae1e
Tom Schenk Jr <tomschenkjr@gmail.com>
2014-09-01 21:22:24 -0500

    Adjusted bio language

--- front-page.php
+++ front-page.php
@@ -10,9 +10,9 @@
 <div class="center">
 	<h1>Tom Schenk Jr.</h1>
 	<h2>Nuts and bolts of research and policy in social sciences</h2>
-	<p>I work with data. My life's work has been using data to impact and improve the daily life of people. I am currently the Director of Analytics and Performance Management with the City of Chicago. I write on my <a href="http://tomschenkjr.net/blog">blog</a> about my current focus on open data, research, and data visualization and also write at <a href="www.datanouveau.net">Data Nouveau</a>, a site dedicated to data visualization. You can find me at the following sites.</p>
+	<p>I work with data. My work has been using data to impact and improve the daily life of people. I am currently the Director of Analytics and Performance Management with the City of Chicago. I write on my <a href="http://tomschenkjr.net/blog">blog</a> about my current focus on open data, research, and data visualization and also write at <a href="www.datanouveau.net">Data Nouveau</a>, a site dedicated to data visualization. You can find me at the following sites.</p>
 	<ul id="links">
-		<li id="blog"><a href="http://tomschenkjr.net/index.php"></a></li>
+		<li id="blog"><a href="http://tomschenkjr.net/blog"></a></li>
 		<li id="twitter"><a href="http://twitter.com/tomschenkjr"></a></li>
 		<li id="contact"><a href="http://tomschenkjr.net/contact"></a></li>
 		<li id="linkedin"><a href="https://www.linkedin.com/profile/view?id=330468838&trk=spm_pic"></a></li>
